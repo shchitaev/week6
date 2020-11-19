6d14dfc0dc000486c767b217dd09a359e39cd8f6
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 00:55:43 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+export default (express, bodyParser, fs, crypto, http) => {
+    const app = express();
+
+    app
+    .get('/login/', (req, res) => res.send('eliasgoss'))
+    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
+    //
+    return app;
+}
