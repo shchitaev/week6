c6cd49053ee6d9bfbe4de4b27f3b068960e42706
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 17:28:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,5 @@ import http from 'http';
 import appSrc from './app.js';
 
 const app = appSrc(express, bodyParser, fs, crypto, http);
-const author = 'itmo287704'
-
-res.setHeader('X-Author', author)
-res.setHeader('Access-Control-Allow-Origin', '*')
-res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
 
 app.listen(process.env.PORT ?? 4321);    
