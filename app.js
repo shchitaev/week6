7c09f1170a404f9223cdca7cd5fb6a39eb0b7d12
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 17:34:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 export default (express, bodyParser, fs, crypto, http) => {
     const app = express();
     const author = 'itmo287704'
-  
+
     app
-    res.setHeader('X-Author', author)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     .get('/login/', (req, res) => res.send(author))
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
     //
