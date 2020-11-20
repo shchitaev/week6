91571fa28848584997da80d5604340881af236fd
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 18:27:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ export default (express, bodyParser, fs, crypto, http) => {
     app
     .get('/login/', (req, res) => res.send(author))
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
-    .all('*', (req, res) => res.send('author'));
+    .get('*', (req, res) => res.send('author'));
     //
     return app;
 }
