dd5dabf187e158ac80702a8f56eb5817c469d3c9
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 16:35:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 export default (express, bodyParser, fs, crypto, http) => {
     const app = express();
+    const author = 'itmo287704'
+
+    res.setHeader('X-Author', author)
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
 
     app
     .get('/login/', (req, res) => res.send('itmo287704'))
