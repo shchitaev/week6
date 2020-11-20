db45bcb0932cabb8ff4e67808c0f4486c86163fe
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-20 16:35:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ export default (express, bodyParser, fs, crypto, http) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
 
     app
-    .get('/login/', (req, res) => res.send('itmo287704'))
+    .get('/login/', (req, res) => res.send(author))
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
-    .all('*', (req, res) => res.send('itmo287704'))
+    .all('*', (req, res) => res.send(author))
     //
     return app;
 }
